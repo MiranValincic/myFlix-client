13a123768980d83e3dfbb96f2e337c4bf7c085b1
MiranValincic <80262902+MiranValincic@users.noreply.github.com>
2022-03-01 14:11:34 +0100

    update

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class MainView extends React.Component {
+    render () {
+        return (
+            <div className="main-view">
+                <div>Iron Man</div>
+                <div>Iron Man 2</div>
+                <div>Thor</div>
+            </div>
+        );
+    }
+}
+
+export default MainView;
