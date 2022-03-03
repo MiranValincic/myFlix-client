6ad59ae609daee3438753bda06f8f2f5a4bb3b50
MiranValincic <80262902+MiranValincic@users.noreply.github.com>
2022-03-03 13:39:43 +0100

    fix

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,10 +5,10 @@ import { MovieView } from '../movie-view/movie-view';
 export class MainView extends React.Component {
 
     constructor(){
-        super();
+      super();
         this.state = {
           movies: [
-            { _id: 1, Title: 'Iron Man', Description: 'Iron Man tells the story of Tony Stark, a billionaire industrialist and genius inventor who is kidnapped and forced to build a devastating weapon. Instead, using his intelligence and ingenuity, Tony builds a high-tech suit of armor and escapes captivity.', Genre: 'Action', Director: 'John Favreau', ImagePath: '...'},
+            { _id: 1, Title: 'Iron Man', Description: 'Iron Man tells the story of Tony Stark, a billionaire industrialist and genius inventor who is kidnapped and forced to build a devastating weapon. Instead, using his intelligence and ingenuity, Tony builds a high-tech suit of armor and escapes captivity.', Genre: 'Action', Director: 'John Favreau', ImagePath: ''},
             { _id: 2, Title: 'Iron Man2', Description: 'After Tony Stark announces to the World that he is Iron Man. He faces many problems in his Life, His Lifesource is Poisoning him, The US Government wants his tech, and someone is out to kill Stark.', Genre: 'Action', Director: 'John Favreau', ImagePath: '...'},
             { _id: 3, Title: 'Thor', Description: 'In Norse mythology, Thor (/θɔːr/; from Old Norse: Þórr [ˈθoːrː]) is a hammer-wielding god associated with lightning, thunder, storms, sacred groves and trees, strength, the protection of mankind, hallowing, and fertility. ... By way of Odin, Thor has numerous brothers, including Baldr.', Genre: 'Superhero', Director: 'Alan Taylor', ImagePath: '...'}
           ],
@@ -37,7 +37,7 @@ export class MainView extends React.Component {
             }
           </div>
         );
-      }
+     }
 }
 
 export default MainView;
